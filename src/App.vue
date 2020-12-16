4fc38169b6e9db98b790d6b44203b58ffbcea486
KaYo <kayoczl@163.com>
2020-12-16 16:29:19 +0800

    Travel

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <keep-alive exclude="Detail">
+      <!-- 显示的是当前路由地址所对应的内容 -->
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
